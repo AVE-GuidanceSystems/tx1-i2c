128a46911bc025d090f19d445d6e83b31ba98786
Christian Aguilar <christian.aguilar614@gmail.com>
2017-03-20 12:50:25 -0700

    Header File added

---
+++ src/drv2605.cpp
@@ -0,0 +1,45 @@
+/*
+ * drv2605.cpp
+ *
+ *  Created on: Mar 20, 2017
+ *      Author: christian
+ */
+
+drv2605::drv2605(){
+
+}
+boolean drv2605::begin(){
+
+}
+
+void writeRegistar8(unit8_t reg, uint8_t val){
+
+}
+void setWaveform(uint8_t slot, uint8_t w){
+
+}
+  void selectLibrary(uint8_t lib){
+
+  }
+  void go(){
+
+  }
+  void stop(){
+
+  }
+  void setMode(uint8_t mode){
+
+  }
+  void setRealtimeValue(uint8_t rtp){
+
+  }
+  // Select ERM (Eccentric Rotating Mass) or LRA (Linear Resonant Actuator) vibration motor
+  // The default is ERM, which is more common
+  void useERM(){
+
+  }
+  void useLRA(){
+
+  }
+
+
---
+++ src/drv2605.h
@@ -0,0 +1,90 @@
+/*
+ * drv2605.h
+ *
+ *  Created on: Mar 20, 2017
+ *      Author: christian
+ */
+
+#ifndef DRV2605_H_
+#define DRV2605_H_
+
+#include <cstddef>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <cstdlib>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define DRV2605_ADDR 0x5A
+
+#define DRV2605_REG_STATUS 			0x00
+#define DRV2605_REG_MODE 			0x01
+#define DRV2605_MODE_INTTRIG  		0x00
+#define DRV2605_MODE_EXTTRIGEDGE  	0x01
+#define DRV2605_MODE_EXTTRIGLVL  	0x02
+#define DRV2605_MODE_PWMANALOG  	0x03
+#define DRV2605_MODE_AUDIOVIBE  	0x04
+#define DRV2605_MODE_REALTIME  		0x05
+#define DRV2605_MODE_DIAGNOS  		0x06
+#define DRV2605_MODE_AUTOCAL  		0x07
+
+
+#define DRV2605_REG_RTPIN 			0x02
+#define DRV2605_REG_LIBRARY 		0x03
+#define DRV2605_REG_WAVESEQ1 		0x04
+#define DRV2605_REG_WAVESEQ2 		0x05
+#define DRV2605_REG_WAVESEQ3 		0x06
+#define DRV2605_REG_WAVESEQ4 		0x07
+#define DRV2605_REG_WAVESEQ5 		0x08
+#define DRV2605_REG_WAVESEQ6 		0x09
+#define DRV2605_REG_WAVESEQ7 		0x0A
+#define DRV2605_REG_WAVESEQ8 		0x0B
+
+#define DRV2605_REG_GO 				0x0C
+#define DRV2605_REG_OVERDRIVE 		0x0D
+#define DRV2605_REG_SUSTAINPOS 		0x0E
+#define DRV2605_REG_SUSTAINNEG 		0x0F
+#define DRV2605_REG_BREAK 			0x10
+#define DRV2605_REG_AUDIOCTRL 		0x11
+#define DRV2605_REG_AUDIOLVL 		0x12
+#define DRV2605_REG_AUDIOMAX 		0x13
+#define DRV2605_REG_RATEDV 			0x16
+#define DRV2605_REG_CLAMPV 			0x17
+#define DRV2605_REG_AUTOCALCOMP 	0x18
+#define DRV2605_REG_AUTOCALEMP	 	0x19
+#define DRV2605_REG_FEEDBACK 		0x1A
+#define DRV2605_REG_CONTROL1 		0x1B
+#define DRV2605_REG_CONTROL2 		0x1C
+#define DRV2605_REG_CONTROL3 		0x1D
+#define DRV2605_REG_CONTROL4 		0x1E
+#define DRV2605_REG_VBAT 			0x21
+#define DRV2605_REG_LRARESON 		0x22
+
+class drv2605 {
+public:
+	unsigned char kI2CBus;         // I2C bus of the Lidar-Lite
+	int kI2CFileDescriptor;        // File Descriptor to the Lidar-Lite
+	int error;
+
+	drv2605(void);
+	boolean begin(void);
+
+	void writeRegistar8(unit8_t reg, uint8_t val);
+	void setWaveform(uint8_t slot, uint8_t w);
+	  void selectLibrary(uint8_t lib);
+	  void go(void);
+	  void stop(void);
+	  void setMode(uint8_t mode);
+	  void setRealtimeValue(uint8_t rtp);
+	  // Select ERM (Eccentric Rotating Mass) or LRA (Linear Resonant Actuator) vibration motor
+	  // The default is ERM, which is more common
+	  void useERM();
+	  void useLRA();
+
+private:
+
+};
+
+#endif /* DRV2605_H_ */
