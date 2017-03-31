7efd15718885292aecbf5e509122ce0e0700f86d
Morales178 <morales.juan.m.178@gmail.com>
2017-03-30 17:29:45 -0700

    example of motor code from arduino sketch

--- src/i2c-tx1.cpp
+++ src/i2c-tx1.cpp
@@ -7,9 +7,26 @@
 //============================================================================
 
 #include <iostream>
+#include "drv2605.h"
 using namespace std;
 
+drv2605 drv;
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    drv.begin();
+    drv.selectLibrary(1);
+    drv.setMode(DRV2605_MODE_INTTRIG);
+    
+    
+    while(1){
+        drv.setWaveform(0, 47); //selects specified waveform
+        drv.setWaveform(1,0);   //ends waveform
+        drv.go();               //starts effect
+        
+        usleep(500);     //milliseconds
+        
+        
+    }//while
+    
 	return 0;
 }
